4c0f07dfb332f23e69e4072f131e7f0137a129e2
jeanne magpantay <jeanne@gmail.com>
2019-04-23 22:02:22 +0800

    some footer and stuff

--- home/views.py
+++ home/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from users.models import Registered_User
 
+from django.views.generic import View
+from django.shortcuts import render
+
 '''
 Passes to home/main.html the username of the current registered user and his type contained in the table Registered_users
 '''
 
 
 def main(request):
-
     return render(request, 'home/main.html')
+
